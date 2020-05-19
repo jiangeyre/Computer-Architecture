0bb403aff3b58b31b7e3c0a6532122b1714e92be
Lexie Jiang <lexiemingjiang@gmail.com>
2020-05-19 08:45:44 -0700

    Commit 2: Day 1 MVP done

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,12 +2,21 @@
 
 import sys
 
+# `HLT` instruction handler
+HLT = 1
+# `LDI` instruction handler
+LDI = 130
+# `PRN` instruction handler
+PRN = 71
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.ram = [0] * 256
+        self.reg = [0] * 8
+        self.pc = 0
 
     def load(self):
         """Load a program into memory."""
@@ -36,7 +45,7 @@ class CPU:
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        # elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -48,8 +57,8 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
+            # self.fl,
+            # self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -60,6 +69,36 @@ class CPU:
 
         print()
 
+    def ram_read(self, address):
+        return self.ram[address]
+
+    def ram_write(self, value, address):
+        value = self.ram[address]
+
     def run(self):
         """Run the CPU."""
-        pass
+        self.load()
+
+        while True:
+            # needs to read mem address stores in register PC and store in IR - local variable
+            IR = self.ram_read(self.pc)
+            # read the bytes at `PC+1` and `PC+2` from RAM into variables `operand_a` and `operand_b` in case the instruction needs them
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
+            # Then, depending on the value of the opcode, perform the actions needed for the instruction per the LS-8 spec. Maybe an `if-elif` cascade...? There are other options, too
+
+            if IR == HLT:
+                # In `run()` in your switch, exit the loop if a `HLT` instruction is encountered regardless of whether or not there are more lines of code in the LS-8 program you loaded
+                print("Exit")
+                break
+            elif IR == PRN:
+                # Add the `PRN` instruction
+                data = self.ram[self.pc + 1]
+                print(self.reg[data])
+                self.pc += 2
+            elif IR == LDI:
+                # Add the `LDI` instruction
+                self.reg[operand_a] = operand_b
+                self.pc += 3
+            else:
+                print("Error")
