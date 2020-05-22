f646539135ed0987a7009b7636c091b8a2ba9a9a
Lexie Jiang <lexiemingjiang@gmail.com>
2020-05-22 07:38:44 -0700

    Commit 2: MVP Day4 Met

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,93 +2,165 @@
 
 import sys
 
-'''
-# `HLT` instruction handler
-HLT = 1
-# `LDI` instruction handler
-LDI = 130
-# `PRN` instruction handler
-PRN = 71
-'''
+# opcodes
+LDI = 0b10000010
+PRN = 0b01000111
+HLT = 0b00000001
+ADD = 0b10100000
+MUL = 0b10100010
+POP = 0b01000110
+PUSH = 0b01000101
+CALL = 0b01010000
+RET = 0b00010001
+CMP = 0b10100111
+JMP = 0b01010100
+JEQ = 0b01010101
+JNE = 0b01010110
 
 SP = 7
 
-HLT = 0b00000001
-PRN = 0b01000111
-LDI = 0b10000010
-ADD = 0b10100000
-MUL = 0b10100010
-PUSH = 0b01000101
-POP = 0b01000110
+# Flags
+LT = 0b00000100
+GT = 0b00000010
+EQ = 0b00000001
+
 
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        self.ram = [0] * 256
-        self.reg = [0] * 8
         self.pc = 0
-        self.reg[SP] = 0XF4
+        self.reg = [0] * 8
+        self.ram = [0] * 256
+        self.fl = 0
+        self.bt = {
+            LDI: self.op_ldi,
+            PRN: self.op_prn,
+            HLT: self.op_hlt,
+            ADD: self.op_add,
+            MUL: self.op_mul,
+            POP: self.op_pop,
+            PUSH: self.op_push,
+            CALL: self.op_call,
+            RET: self.op_ret,
+            CMP: self.op_cmp,
+            JMP: self.op_jmp,
+            JEQ: self.op_jeq,
+            JNE: self.op_jne
+        }
 
-    def load(self):
+    def op_ldi(self, operand_a, operand_b):
+        self.reg[operand_a] = operand_b
+
+    def op_prn(self, operand_a, operand_b):
+        print(self.reg[operand_a])
+
+    def op_hlt(self, operand_a, operand_b):
+        sys.exit(0)
+
+    def op_add(self, operand_a, operand_b):
+        self.alu("ADD", operand_a, operand_b)
+
+    def op_mul(self, operand_a, operand_b):
+        self.alu("MUL", operand_a, operand_b)
+
+    def op_pop(self, operand_a, operand_b):
+        self.reg[operand_a] = self.pop_val()
+
+    def op_push(self, operand_a, operand_b):
+        self.push_val(self.reg[operand_a])
+
+    def op_call(self, operand_a, operand_b):
+        # Pushing next instruction to stack
+        self.push_val(self.pc + 2)
+        # Setting pc to address of subroutine
+        self.pc = self.reg[operand_a]
+
+    def op_ret(self, operand_a, operand_b):
+        # Poping next instruction off top of stack
+        self.pc = self.pop_val()
+
+    def op_cmp(self, operand_a, operand_b):
+        self.alu("CMP", operand_a, operand_b)
+
+    def op_jmp(self, operand_a, operand_b):
+        # Set the PC to the address stored in the given register
+        self.pc = self.reg[operand_a]
+
+    def op_jeq(self, operand_a, operand_b):
+        # If equal flag is set (true), jump to the address stored in the given register
+        if self.fl == EQ:
+            self.pc = self.reg[operand_a]
+        else:
+            self.pc += 2
+
+    def op_jne(self, operand_a, operand_b):
+        # If E flag is clear (false, 0), jump to the address stored in the given register
+        if not self.fl == EQ:
+            self.pc = self.reg[operand_a]
+        else:
+            self.pc += 2
+
+    def load(self, filename):
         """Load a program into memory."""
-
-        address = 0
-
         try:
-            filename = sys.argv[1]
             address = 0
-            with open(filename) as f:
-                # read the comments in line by line
+            # Open the file
+            with open(sys.argv[1]) as f:
+                # Read all the lines
                 for line in f:
-                    # remove any comment
-                    line = line.split("#")[0]
-                    # remove the white space
-                    line = line.strip()
-                    # skip over the empty lines
-                    if line == "":
+                    # Parse out the comments
+                    comment_split = line.strip().split("#")
+                    # Cast number strings to ints
+                    value = comment_split[0].strip()
+                    # Ignore blank lines
+                    if value == "":
                         continue
-
-                    value = int(line, 2)
-
-                    # set the instruction to the mem
-                    self.ram[address] = value
+                    instruction = int(value, 2)
+                    # Populate a memory array
+                    self.ram[address] = instruction
                     address += 1
 
         except FileNotFoundError:
             print("File not found")
             sys.exit(2)
 
+    def ram_read(self, mar):
+        # Accept the address to read (mar) and return
+        # address to write (mdr)
+        mdr = self.ram[mar]
+        return mdr
+
+    def ram_write(self, mdr, mar):
+        # Accept a value to write (mdr), and address
+        # to write it to (mar)
+        self.ram[mar] = mdr
+
+    def push_val(self, val):
+        self.reg[SP] -= 1
+        self.ram_write(val, self.reg[SP])
+
+    def pop_val(self):
+        val = self.ram_read(self.reg[SP])
+        self.reg[SP] += 1
+        return val
+
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        # elif op == "SUB": etc
         elif op == "MUL":
             self.reg[reg_a] *= self.reg[reg_b]
-        if op == 'OR':
-            if reg_a == 1 and reg_b == 0:
-                return True
-            elif reg_a == 0 and reg_b == 1:
-                return True
-            elif reg_a == 1 and reg_b == 1:
-                return True
+        elif op == "CMP":
+            if self.reg[reg_a] < self.reg[reg_b]:
+                self.fl = LT
+            elif self.reg[reg_a] > self.reg[reg_b]:
+                self.fl = GT
             else:
-                return False
-        if op == 'XOR':
-            if reg_a == 1 and reg_b == 0:
-                return True
-            if reg_a == 0 and reg_b == 1:
-                return True
-            else:
-                return False
-        if op == 'NOR':
-            if reg_a == 0 and reg_b == 0:
-                return True
-            else:
-                return False
+                self.fl = EQ
+        # elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -100,7 +172,7 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            # self.fl,
+            self.fl,
             # self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
@@ -112,65 +184,17 @@ class CPU:
 
         print()
 
-    def ram_read(self, address):
-        return self.ram[address]
-
-    def ram_write(self, value, address):
-        value = self.ram[address]
-
     def run(self):
         """Run the CPU."""
-        self.load()
-
         while True:
-            # needs to read mem address stores in register PC and store in IR - local variable
-            IR = self.ram_read(self.pc)
-            # read the bytes at `PC+1` and `PC+2` from RAM into variables `operand_a` and `operand_b` in case the instruction needs them
+            instruction = self.ram[self.pc]
             operand_a = self.ram_read(self.pc + 1)
             operand_b = self.ram_read(self.pc + 2)
-            # Then, depending on the value of the opcode, perform the actions needed for the instruction per the LS-8 spec. Maybe an `if-elif` cascade...? There are other options, too
-
-            if IR == HLT:
-                # In `run()` in your switch, exit the loop if a `HLT` instruction is encountered regardless of whether or not there are more lines of code in the LS-8 program you loaded
-                print("Exit")
-                sys.exit(-1)
-                break
-            elif IR == PRN:
-                # Add the `PRN` instruction
-                data = operand_a
-                print(self.reg[data])
-                self.pc += 2
-            elif IR == LDI:
-                # Add the `LDI` instruction
-                self.reg[operand_a] = operand_b
-                self.pc += 3
-            elif IR == ADD:
-                self.alu("ADD", operand_a, operand_b)
-                self.pc += 3
-            elif IR == MUL:
-                self.alu("MUL", operand_a, operand_b)
-                self.pc += 3
-            elif IR == PUSH:
-                # choose register
-                reg = self.ram[self.pc+1]
-                # get value from register
-                val = self.reg[reg]
-                # decrement memory address by one
-                self.reg[SP] -= 1
-                # vave value from register into memory
-                self.ram[self.reg[SP]] = val
-                # increment pc by 2
-                self.pc += 2
-            elif IR == POP:
-                # reg holding sp
-                reg = self.ram[self.pc+1]
-                # value from place in memory
-                val = self.ram[self.reg[SP]]
-                # save value into register we arelooking at
-                self.reg[reg] = val
-                # increment pointer
-                self.reg[SP] += 1
-                # increment pc by 2
-                self.pc += 2
-            else:
-                print("Error")
+            instruction_length = (instruction >> 6) + 1
+            instruction_set_pc = ((instruction >> 4) & 0b1) == 1
+            if instruction in self.bt:
+                self.bt[instruction](operand_a, operand_b)
+            # Check if opcode sets the pc
+            # If not, increment pc by instruction_length
+            if instruction_set_pc != 1:
+                self.pc += instruction_length
